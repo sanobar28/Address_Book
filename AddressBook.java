d26bf1ed59e9d37b6f5de0cca5f7bf0fcc104077
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-07 17:13:15 +0530

    Welcome to address book

--- AddressBook.java
+++ AddressBook.java
@@ -3,7 +3,9 @@ package com.bridgelabz.JavaPractice;
 public class AddressBook {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("Welcome to address book syytem");
 
 	}
 
