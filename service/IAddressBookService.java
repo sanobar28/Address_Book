40e40bbf98ad855f21cade5da23dc72b9abd30f2
sanobar28 <sanobar.mujawar@gmail.com>
2021-06-28 22:00:32 +0530

    added: person deatils written in JSON file

--- service/IAddressBookService.java
+++ service/IAddressBookService.java
@@ -1,5 +1,20 @@
 package com.bridgelabz.JavaPractice.service;
 
+import com.bridgelabz.JavaPractice.exception.AddressBookException;
+
 public interface IAddressBookService {
 
+	void addRecord();
+
+	void editRecord() throws AddressBookException;
+
+	void displayRecord();
+
+	void deleteRecord() throws AddressBookException;
+
+	void sortRecords();
+
+	void searchInRecords();
+
+	boolean checkExists(String firstName);
 }
